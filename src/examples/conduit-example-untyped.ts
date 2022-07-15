b0fc0958fd423594430c20cd1bf9fd61b0669b8d
PVillela <pvillela@gmail.com>
2022-07-15 19:30:07 -0400

    Cleaned-up typing in conduit examples.

--- src/examples/conduit-example-untyped.ts
+++ src/examples/conduit-example-untyped.ts
@@ -3,8 +3,8 @@ import { parseCollection } from "@src/lib/parse-collection-untyped";
 import { template } from "./template";
 import { CollectionDefinition } from "postman-collection";
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
-const coll: CollectionDefinition = require("../../postman-collections/Conduit.postman_collection.json")
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const coll = require("../../postman-collections/Conduit.postman_collection.json") as CollectionDefinition
 
 parseCollection(config.outDir, coll, template, 3).catch((err) => {
   throw err
